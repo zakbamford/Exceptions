41a5ae1e72d5bc7164036c575acf21f7392607ad
DaveDLeague <dave.dunn@jointheleague.org>
2018-02-13 10:15:29 -0800

    Update ExceptionsTest.java

--- src/_02_gotta_catchem_all/ExceptionsTest.java
+++ src/_02_gotta_catchem_all/ExceptionsTest.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-//Complete the methods in the ExceptionMethods class to make the tests pass
 
 class ExceptionsTest {
+	ExceptionMethods em = new ExceptionMethods();
+	
+	//1. This is an example of how one might test that an exception is thrown.
+	//   It tests that an IndexOutOfBoundsException is thrown 
 	@Test
 	public void testIndexOOBException() {
 		int[] vars = {0, 1, 2, 3};
@@ -21,6 +24,27 @@ class ExceptionsTest {
 			assertArrayEquals(vars, new int[] {4, 1, 2, 3});
 		}
 	}
+	
+	//2. In the ExceptionMethods class, write a method called divide that takes
+	//   two doubles as parameters and returns their product as a double. This method 
+	//   should throw an IllegalArgumentException if the denominator is 0.0.
+	
+	//3. Complete the JUnit test method to test the divide method.
+	@Test
+	public void testDivideByZeroException() {
+		
+	}
+	
+	//4. In the ExceptionMethods class, write a method called reverseString that takes a
+	//   String and returns the reverse of that String. It should throw an IllegalStateException
+	//   if the String passed in is empty
+	
+	//5. Complte the JUnit test method to test the reverseStringMethod.
+	@Test
+	public void testReverseString(String s) {
+		
+	}
+	
 	
 	
 

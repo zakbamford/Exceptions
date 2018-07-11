493462eb1e6d77d9b8d74cd306d4bd3a4d1bba70
zakbamford <League@AdministorsiMac.attlocal.net>
2018-07-11 14:55:39 -0700

    Finished exceptions intro

--- src/_01_introduction/ExceptionsDemo.java
+++ src/_01_introduction/ExceptionsDemo.java
@@ -16,11 +16,25 @@ public class ExceptionsDemo {
 		//    In the catch block, put a print statement so you know
 		//    if the code is being ran.
 		
+		//try {
+		//	testMethod1(3);
+		//} catch (Exception e) {
+			// TODO Auto-generated catch block
+		//	e.printStackTrace();
+		//}
+		
 		// 2. Now call testMethod1 with a value greater than 5. 
 		//    Running your program should not print the statement
 		//    in the catch block.
 		//    You can use the Exception class's printStackTrace() method
 		//    to print a list of methods that are currently in scope.
+		
+		//try {
+			//testMethod1(7);
+		//} catch (Exception e) {
+			// TODO Auto-generated catch block
+		//	e.printStackTrace();
+		//}
 		
 		// 3. Create a new class called CustomException that extends Exception.
 		//    Add a void method called terminate that simply calls System.exit(0);
@@ -30,11 +44,31 @@ public class ExceptionsDemo {
 		//    CustomException object.
 		//    This demonstrates how custom exception classes can be used to
 		//    handle exceptions in unlimited ways.
+		
+		try {
+			testMethod2(-7);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public static void testMethod1(int x) throws Exception {
 		if(x < 5) {
 			throw new Exception();
+		}
+	}
+	
+	public static void testMethod2(int x) throws CustomException {
+		if (x < 0) {
+			throw new CustomException();
+		}
+	}
+	
+	public static class CustomException extends Exception {
+		public void terminate() {
+			System.exit(0);
 		}
 	}
 	

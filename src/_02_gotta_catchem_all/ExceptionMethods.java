8bc1472ee5332bd9b94f3d0318b7b495fe86860c
zakbamford <league@WTS-iMac-11.attlocal.net>
2018-07-12 14:14:30 -0700

    ExceptionsTest/ExceptionMethods

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,21 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+	public static double divide(double numerator, double denominator) throws IllegalArgumentException {
+		if (denominator == 0)
+			throw new IllegalArgumentException();
+		else
+			return numerator / denominator;
+	}
+
+	public static String reverseString(String str) throws IllegalStateException {
+		String str2 = "";
+		if (str.length() == 0)
+			throw new IllegalStateException();
+		else {
+			for (int i = str.length() - 1; i >= 0; i++)
+				str2 += str.charAt(i);
+			return str2;
+		}
+	}
 }
--- src/_02_gotta_catchem_all/ExceptionsTest.java
+++ src/_02_gotta_catchem_all/ExceptionsTest.java
@@ -4,48 +4,55 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-
 class ExceptionsTest {
 	ExceptionMethods em = new ExceptionMethods();
-	
-	//1. This is an example of how one might test that an exception is thrown.
-	//   It tests that an IndexOutOfBoundsException is thrown 
+
+	// 1. This is an example of how one might test that an exception is thrown.
+	// It tests that an IndexOutOfBoundsException is thrown
 	@Test
 	public void testIndexOOBException() {
-		int[] vars = {0, 1, 2, 3};
-			
+		int[] vars = { 0, 1, 2, 3 };
+
 		try {
 			vars[0] = 4;
-			assertArrayEquals(vars, new int[] {4, 1, 2, 3});
+			assertArrayEquals(vars, new int[] { 4, 1, 2, 3 });
 			vars[4] = 5;
 			fail("IndexOutOfBoundsException not thrown");
-		}catch(IndexOutOfBoundsException e) {
+		} catch (IndexOutOfBoundsException e) {
 			e.printStackTrace();
-			assertArrayEquals(vars, new int[] {4, 1, 2, 3});
+			assertArrayEquals(vars, new int[] { 4, 1, 2, 3 });
 		}
 	}
-	
-	//2. In the ExceptionMethods class, write a method called divide that takes
-	//   two doubles as parameters and returns their quotient as a double. This method 
-	//   should throw an IllegalArgumentException if the denominator is 0.0.
-	
-	//3. Complete the JUnit test method to test the divide method.
+
+	// 2. In the ExceptionMethods class, write a method called divide that takes
+	// two doubles as parameters and returns their quotient as a double. This method
+	// should throw an IllegalArgumentException if the denominator is 0.0.
+
+	// 3. Complete the JUnit test method to test the divide method.
 	@Test
 	public void testDivideByZeroException() {
-		
+		try {
+			ExceptionMethods.divide(9, 0);
+			fail("IllegalArgumentException not thrown");
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	//4. In the ExceptionMethods class, write a method called reverseString that takes a
-	//   String and returns the reverse of that String. It should throw an IllegalStateException
-	//   if the String passed in is empty
-	
-	//5. Complete the JUnit test method to test the reverseStringMethod.
+
+	// 4. In the ExceptionMethods class, write a method called reverseString that
+	// takes a
+	// String and returns the reverse of that String. It should throw an
+	// IllegalStateException
+	// if the String passed in is empty
+
+	// 5. Complete the JUnit test method to test the reverseStringMethod.
 	@Test
 	public void testReverseString() {
-		
+		try {
+			ExceptionMethods.reverseString("");
+			fail("IllegalStateException not thrown");
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	
-	
-
 }
